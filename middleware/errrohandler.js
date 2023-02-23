2e15fce088b96764e2c610b65e81048ad0fd218e
RaCoDev <itsmerana7@gmail.com>
2023-02-23 15:36:42 +0530

    handling validators

--- middleware/errrohandler.js
+++ middleware/errrohandler.js
@@ -1,10 +1,31 @@
+const ErrorResponse = require("../utils/errorResponse");
+
+
 const errorHandler=(err,req,res,next)=>{
     console.log(err.stack.red)
-    res.status(500).json({
+    let error={...err}
+    error.message=err.message;
+    console.log(err)
+    if(err.name===11000){
+        const message=`Duplicate key value added`
+        error=new ErrorResponse(message,400)
+    }
+    if(err.name==='CastError'){
+        const message=`Book With this id not found ${err.value}`
+        error=new ErrorResponse(message,404)
+    }
+
+    if(err.name==='ValidationError'){
+        const message=Object.values(err.errors).map(value=>value.message)
+        error=new ErrorResponse(message,404)
+    }
+
+    res.status(error.statusCode || 500).json({
         success:false,
-        error:err.message
+        error:error.message || "Server Error"
     })
 
+    
 
 }
 
--- models/bookModel.js
+++ models/bookModel.js
@@ -3,13 +3,14 @@ const { Schema,model } = require("mongoose");
 const BookSchema = new Schema({
   title: {
     type: String,
-    require: true,
+    required: [true,'please enter title'],
     unique: true,
     trim: true,
+    minlength:[8,'not more thn 10 character'],
     maxlength: [50, "Not more than 50 character"],
     lowercase:true
   },
-  author: { type: String, lowercase:true },
+  author: { type: String,required:[true,'please enter the author'],lowercase:true },
   price: { type: Number },
   slug: { type: String },
 });
